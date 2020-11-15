2f82ea8ca8481cc52a248102894c1bb702903cf7
sanava2010 <sanava2010@gmail.com>
2020-11-15 15:25:30 -0600

    Added data

--- src/main/scala/com/group11/hw3/example.scala
+++ src/main/scala/com/group11/hw3/example.scala
@@ -1,7 +1,9 @@
 package com.group11.hw3
 
+import scala.io.Source
+
 class example {
   val server = new akkaHttpServer
   server.startServer()
-
+  val lines = Source.fromFile("/Users/Al/.bash_profile").getLines.toList
 }
--- src/main/scala/com/group11/hw3/main_test.scala
+++ src/main/scala/com/group11/hw3/main_test.scala
@@ -1,5 +1,7 @@
 package com.group11.hw3
 
+import scala.io.Source
+
 
 /*
 Class used to test the basic working of the akka http server.
@@ -8,6 +10,9 @@ object main_test {
   def main(args: Array[String]): Unit = {
     val serv= new akkaHttpServer
     serv.startServer()
+    val lines = Source.fromFile("src/main/resources/listfile.txt").getLines.slice(0,2000).toList
+    println(lines.head)
+    println(lines.size)
   }
 
 }
