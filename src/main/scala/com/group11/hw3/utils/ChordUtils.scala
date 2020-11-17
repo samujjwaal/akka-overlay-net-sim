3e66d4695b1e679b56674ff818f3d70d04d7e2a3
Samujjwaal Dey <50056909+samujjwaal@users.noreply.github.com>
2020-11-16 19:12:56 -0600

    Update md5 hash function

--- src/main/scala/com/group11/hw3/utils/ChordUtils.scala
+++ src/main/scala/com/group11/hw3/utils/ChordUtils.scala
@@ -1,9 +1,10 @@
 package com.group11.hw3.utils
 
+import java.math.BigInteger
 import java.security.MessageDigest
 
 object ChordUtils {
 
-  def md5(s: String): String = MessageDigest.getInstance("MD5").digest(s.getBytes("UTF-8")).map("%02x".format(_)).mkString
-  
+  def md5(s: String): BigInt = new BigInteger(1,MessageDigest.getInstance("MD5").digest(s.getBytes("UTF-8")))
+
 }
