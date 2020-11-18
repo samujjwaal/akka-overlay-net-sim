1dd1b066b30d00e6b91911a9f9297ddccc7011ed
Garima <ggupta22@uic.edu>
2020-11-17 21:33:39 -0600

    delete extra files

--- src/main/scala/com/group11/hw3/example.scala
+++
@@ -1,9 +0,0 @@
-package com.group11.hw3
-
-import scala.io.Source
-
-class example {
-  val server = new akkaHttpServer
-  server.startServer()
-  val lines = Source.fromFile("/Users/Al/.bash_profile").getLines.toList
-}
