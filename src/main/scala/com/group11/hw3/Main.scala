7aa3448da4b66f7339e2e03cba7e99c9327ec063
Samujjwaal Dey <50056909+samujjwaal@users.noreply.github.com>
2020-11-18 18:45:16 -0600

    Reorganized files

--- src/main/scala/com/group11/hw3/Main.scala
+++ src/main/scala/com/group11/hw3/Main.scala
@@ -6,14 +6,14 @@ import akka.actor.typed.ActorSystem
 /*
 Class used to test the basic working of the akka http server.
  */
-object main_test {
+object Main {
   def main(args: Array[String]): Unit = {
 
-    val sys=ActorSystem(serverRedone(),"ChordServerSystem")
+    val sys = ActorSystem(ChordHttpServer(),"ChordServerSystem")
     Thread.sleep(2000)
-//    println("Starting user requests now...")
-//    ActorSystem(UserSystem(),"Users")
-    sys ! CaptureGlobalSnapshot()
+    println("Starting user requests now...")
+    ActorSystem(UserSystem(),"Users")
+//    sys ! CaptureGlobalSnapshot()
 
   }
 
--- src/main/scala/com/group11/hw3/NodeConstants.scala
+++ src/main/scala/com/group11/hw3/NodeConstants.scala
@@ -3,5 +3,5 @@ package com.group11.hw3
 object NodeConstants {
   val numNodes = 2
   val M = 128
-//  val ringSize: Int = pow(2,M).toInt
+  val ringSize: BigInt = BigInt(2).pow(M)
 }
