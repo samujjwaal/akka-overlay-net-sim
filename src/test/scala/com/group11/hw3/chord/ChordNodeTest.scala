5e440036311e6a47115fe4df46507a6286e75fca
Samujjwaal Dey <50056909+samujjwaal@users.noreply.github.com>
2020-11-18 20:25:32 -0600

    Update test cases

--- src/test/scala/com/group11/hw3/chord/ChordNodeTest.scala
+++ src/test/scala/com/group11/hw3/chord/ChordNodeTest.scala
@@ -10,38 +10,27 @@ class ChordNodeTest extends AnyFunSuite {
 
 
 
-  test("") {
+  test("Chord Node Actor") {
     object TestChordSystem {
       def apply(): Behavior[NodeCommand] = Behaviors.setup { context =>
 
         val node1 = context.spawn(ChordNode(md5("Node1")), "Node1")
-//        node1 ! DisplayNodeInfo()
+
+        assert(node1!=null)
         assert(node1.isInstanceOf[ActorRef[NodeCommand]])
+
         Thread.sleep(100)
 
         val node2 = context.spawn(ChordNode(md5("Node2")), "Node2")
-//        node1 ! SetSuccessor(node2)
-        //        node1 ! FindSuccessor()
-//        node2 ! DisplayNodeInfo()
-        //        node2 ! FindSuccessor()
-        Thread.sleep(100)
-//        node1 ! DisplayNodeInfo()
 
-        //        val node3 = context.spawn(ChordNode2(md5("Node3")),"Node3")
-        //        node2 ! SetSuccessor(node3)
-        //        node2 ! FindSuccessor()
-        //        node3 ! DisplayNodeInfo()
-        //        node3 ! FindSuccessor()
-        Thread.sleep(100)
-//        node2 ! DisplayNodeInfo()
-        //    val node4 = context.spawn(ChordNode2(md5("Node4")),"Node4")
-        //    node4 ! DisplayNodeInfo()
+        assert(node1.path != node2.path)
 
         Behaviors.stopped
       }
     }
 
     val chordSystem = ActorSystem(TestChordSystem(),"TestChordSystem")
+    
     assert(chordSystem.isInstanceOf[ActorSystem[NodeCommand]])
 
   }
