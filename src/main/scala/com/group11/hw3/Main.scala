e7cf68a0db4540449e03daa8c00c04d6b77b0b28
Garima <ggupta22@uic.edu>
2020-12-09 03:39:00 -0600

    Chord User request simulation

--- src/main/scala/com/group11/hw3/Main.scala
+++ src/main/scala/com/group11/hw3/Main.scala
@@ -9,14 +9,13 @@ import com.group11.hw3.chord.ChordClassicNode
 import com.typesafe.config.{Config, ConfigFactory}
 import scalaj.http.{ HttpOptions, HttpRequest}
 
-import scala.collection.mutable.{ArrayBuffer, ListBuffer}
+import scala.collection.mutable.ListBuffer
 import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.language.postfixOps
-import scala.util.Random
 
-/*
-Class used to test the basic working of the akka http server.
+/**
+ * Class used to test the basic working of the akka http server.
  */
 object Main {
   def main(args: Array[String]): Unit = {
@@ -26,7 +25,9 @@ object Main {
     val netConf = conf.getConfig("networkConstants")
     val userConf = conf.getConfig("userConstants")
 
-    // Create Chord System
+    /*
+     Create Chord System
+     */
     implicit val chordSystem = ActorSystem(netConf.getString("networkSystemName"))
     val chordNodesId = new ListBuffer[BigInt]()
     val chordShardRegion: ActorRef = ClusterSharding(chordSystem).start(
@@ -37,7 +38,9 @@ object Main {
       extractShardId = ChordClassicNode.extractShardId
     )
 
-    // Add first node to Chord network
+    /*
+     Add first node to Chord network
+     */
     var hashID = BigInt(0)
     val peer=hashID
     implicit val timeout = Timeout(15 seconds)
@@ -45,7 +48,9 @@ object Main {
     val joinStatus = Await.result(future,timeout.duration).asInstanceOf[CJoinStatus]
     chordNodesId += hashID
 
-    // Add remaining nodes to Chord network
+    /*
+     Add remaining nodes to Chord network
+     */
     while (chordNodesId.size < numNodes) {
       val hashID = BigInt(scala.util.Random.nextInt(conf.getInt("networkConstants.nodeSpace")))
       if (!(chordNodesId.contains(hashID))) {
@@ -68,38 +73,61 @@ object Main {
 //      println("Node : "+i.toString+" FT : "+fingerStatus.ft)
 //    }
 
-    //Writing initial data to nodes
-    val dataList = new ArrayBuffer[Array[String]]()
-    dataList.addOne(Array("1", "1998"))
-    dataList.addOne(Array("7", "2000"))
-    dataList.addOne(Array("14", "1996"))
-    dataList.addOne(Array("20", "1920"))
-    dataList.foreach( data => {
-      val key = data(0)
-      val value = data(1)
-      val randNode= chordNodesId(scala.util.Random.nextInt(chordNodesId.size))
-      chordShardRegion  ! EntityEnvelope(randNode,CWriteKeyValue(BigInt(key), value.toInt))
-    } )
-
-    Thread.sleep(2000)
+    /*
+     Start the HTTP server
+     */
+    Thread.sleep(1000)
     val server = new HTTPServer()
     val r=server.setupServer(chordSystem,chordShardRegion,chordNodesId.toList)
     Http().bindAndHandle(r, "localhost")
 
     Thread.sleep(100)
 
+    /*
+     Create simulation data
+     */
+    val data = new ListBuffer[(BigInt,Int)]()
+    val maxKey = netConf.getInt("maxKey")
+    val totalRecords = netConf.getInt("totalRecords")
+    while (data.size < totalRecords) {
+      val key = scala.util.Random.nextInt(maxKey)
+      val value = scala.util.Random.nextInt(1000)
+      if (!data.contains(key)) {
+        data.addOne((key, value))
+      }
+    }
+
     val request: HttpRequest = scalaj.http.Http("http://localhost:9000/chordRoot")
 
-    val writeResponse = request.params(("name", "23"), ("val", "8907")).method("POST").option(HttpOptions.connTimeout(10000)).asString
-    val readResponse = request.param("name","1").option(HttpOptions.connTimeout(10000)).asString
+    /*
+     Write Initial data to Chord
+     */
+    val recordsToWrite = netConf.getInt("recordsToWrite")
+    var indexWrittenToChord = -1
+    for (i <- 0 until recordsToWrite) {
+      val writeResponse = request.params(("name", data(i)._1.toString), ("val", data(i)._2.toString)).method("POST").option(HttpOptions.connTimeout(10000)).asString
+      indexWrittenToChord = i
+    }
 
+    Thread.sleep(100)
 
-//    val userSystem = ActorSystem(userConf.getString("userSystemName"))
-//    val userMaster = userSystem.actorOf(UserMaster.props(),"user-master")
-//    userMaster ! CreateUsers
-//    userMaster ! StartUserRequests
+    /*
+     Start user request simulation.
+     */
+    val totalRequests = netConf.getInt("totalRequests")
 
-//    sys ! CaptureGlobalSnapshot()
+    for (j <- 0 until totalRequests) {
+      val requestType = scala.util.Random.nextInt(2)
+      if (requestType == 0) {
+        val recordToRead = scala.util.Random.nextInt(indexWrittenToChord+1)
+        val readResponse = request.param("name",data(recordToRead)._1.toString).option(HttpOptions.connTimeout(10000)).asString
+      }
+      else if (indexWrittenToChord < totalRecords-1){
+        val writeResponse = request.params(("name", data(indexWrittenToChord+1)._1.toString), ("val", data(indexWrittenToChord+1)._2.toString)).method("POST").option(HttpOptions.connTimeout(10000)).asString
+        indexWrittenToChord +=1
+        Thread.sleep(100)
+      }
+    }
 
   }
 
