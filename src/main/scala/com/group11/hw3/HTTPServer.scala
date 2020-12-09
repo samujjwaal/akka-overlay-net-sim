cca45e2639ffdff7b500e212676ec307f546b074
sanava2010 <sanava2010@gmail.com>
2020-12-09 00:05:18 -0600

    Chord bug fix in http server

--- src/main/scala/com/group11/hw3/HTTPServer.scala
+++ src/main/scala/com/group11/hw3/HTTPServer.scala
@@ -12,55 +12,34 @@ import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
 
 class HTTPServer {
-
   val conf: Config = ConfigFactory.load("application.conf")
   val netConf = conf.getConfig("networkConstants")
-
   var bindingFuture: Future[Http.ServerBinding] = _
 
   def setupServer(chordSystem: ActorSystem, chordShardRegionRef:ActorRef,chordNodes: List[BigInt]): Route = {
-
-    //implicit val chordSystem: ActorSystem = ActorSystem(netConf.getString("networkSystemName"))
-    //implicit val materializer: ActorMaterializer = ActorMaterializer()
-
-
-    //Define and start http server
+    //Define http server
     val route = path("chordRoot") {
       concat(
         get {
           parameters("name".asInstanceOf[String]) { key =>
-
-            //println("Received read request by HTTP server")
-            //val node = Utils.selectRandomNode(chordSystem, chordNodes)
-            val node = scala.util.Random.nextInt(chordNodes.size)
-            var msgReply = ""
-
+            val node = chordNodes(scala.util.Random.nextInt(chordNodes.size))
             implicit val timeout: Timeout = Timeout(10.seconds)
-            //println("--"+node)
             val future = chordShardRegionRef ? EntityEnvelope(node,CReadKeyValue(key.toInt))
             val readValResp = Await.result(future, timeout.duration).asInstanceOf[CReadResponse]
-            msgReply=readValResp.message
-
-            complete("Read/Get response:"+msgReply)
+            complete("Read/Get response: "+readValResp.message)
           }
         },
         post {
           parameters("name".asInstanceOf[String],"val".asInstanceOf[String]) { (key,value) =>
-
-            //println("Received write request by HTTP server, key:"+key)
-            //val node = Utils.selectRandomNode(chordSystem, chordNodes)
-            val node=scala.util.Random.nextInt(chordNodes.size)
+            val node = chordNodes(scala.util.Random.nextInt(chordNodes.size))
             chordShardRegionRef ! EntityEnvelope(node , CWriteKeyValue(BigInt(key),value.toInt))
-
-            complete("Post method done")
+            complete("Post method done.")
           }
-
         }
       )
     }
     route
 
-    //val bindingFuture = Http().newServerAt("localhost", 9000).bind(route)
   }
 
 }
